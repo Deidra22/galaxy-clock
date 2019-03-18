a5d8bba53d77228cbb9f020d7d60fe274fdd3384
Deidra22 <Deidra22@users.noreply.github.com>
2019-03-17 21:12:20 -0700

    completed script for digital clock

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,35 @@
-//Clock
-
 function clock(){
-    var date = new Date();
-    var hours = date.getHours();
-    var mins = date.getMinutes();
-    var secs = date.getSeconds();
+    var today = new Date();
+    var hr = today.getHours();
+    var min = today.getMinutes();
+    var sec = today.getSeconds();
+    min = checkTime(min);
+    sec = checkTime(sec);
+    document.getElementById('galaxyClock').innerHTML = 
+    hr + ":" + min + ":" + sec;
 
-    ap = (hours < 12) ? "<span>AM</span>" : "<span>PM</span>";
-    hours = (hours == 0) ? 12 : hours;
-    hours = (hours > 12) ? hours - 12 : hours;
-
-    if (hours < 10){
-        hours = "0" + hours;
-    }
-    if (mins < 10) {
-        mins = "0" + mins;
-    }
-    
-    (document.getElementById('hour')).innerHTML = hours;
-    (document.getElementById('minute')).innerHTML = ":" + mins;
-    (document.getElementById('second')).innerHTML = ":" + secs;
+    var t = setTimeout(clock, 500);
 
 
+function checkTime(i){
+    if (i < 10) {i = "0" + i};
+    return i;
 }
-
-setInterval(clock,100);
+    if (hr > 12){
+        hr = hr - 12;
+        if (hr == 12){
+            hr = checkTime(hr);
+            document.getElementById('galaxyclock').innerHTML = 
+            hr + ":" + min + ":" + sec + "AM";
+        }
+        else {
+            hr = checkTime(hr);
+            document.getElementById('galaxyClock').innerHTML = 
+            hr + ":" + min + ":" + sec + "PM";
+        }
+    }
+    else {
+        document.getElementById('galaxyClock').innerHTML = 
+        hr + ":" + min + ":" + sec + "AM";;
+    }
+}
